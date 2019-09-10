c41a7055f341d4d3c92eb75703d6232387554d5d
Dzy <dzy8023@163.com>
2019-09-10 17:58:33 +0800

    变换

--- OpenGL/OpenGL_5_变换/OpenGL_4_纹理/main.cpp
+++ OpenGL/OpenGL_5_变换/OpenGL_4_纹理/main.cpp
@@ -10,6 +10,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "Shader/shader_s.cpp"
 #include "Image/stb_image.h"
 #include <cmath>
@@ -129,11 +133,11 @@ int main(int argc, const char * argv[]) {
     std::cout << "Maximum nr of vertex attributes suppported: " << nrAttributes << std::endl;
     
     float vertices[] = {
-        // ---- 位置 ----      ---- 颜色 ----       - 纹理坐标 -
-         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
-         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
+        // ---- 位置 ----      - 纹理坐标 -
+         0.5f,  0.5f, 0.0f,    1.0f, 1.0f,   // 右上
+         0.5f, -0.5f, 0.0f,    1.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,    0.0f, 0.0f,   // 左下
+        -0.5f,  0.5f, 0.0f,    0.0f, 1.0f    // 左上
     };
     
     // 索引数组
@@ -171,14 +175,11 @@ int main(int argc, const char * argv[]) {
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     // 8 * sizeof(float) 整个顶点属性的步长
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
     
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-    
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
     
     // 所以这里可以安全的解绑
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -194,28 +195,14 @@ int main(int argc, const char * argv[]) {
     
     ourShader.use();
     // 两种赋值方式都可以
-    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+    ourShader.setInt("texture1", 0);
     ourShader.setInt("texture2", 1);
-    ourShader.setFloat("x", 0.2f);
     
     // 5. 准备引擎
     while (!glfwWindowShouldClose(window)) {
         // 检查是否有输入 exc
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(window, true);
-        
-        float x = ourShader.getFloat("x");
-        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-            x += 0.001f;
-            x = (x >= 1.0f) ? 1.0f : x;
-        }
-        
-        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-            x -= 0.001f;
-            x = (x <= 0.0f) ? 0.0f : x;
-        }
-        
-        ourShader.setFloat("x", x);
         
         glClearColor(0.2f, 0.3f, 0.3f, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -226,6 +213,19 @@ int main(int argc, const char * argv[]) {
         glBindTexture(GL_TEXTURE_2D, texture2);
         
         ourShader.use();
+        
+        glm::mat4 trans = glm::mat4(1.0f);
+        // 沿 z 轴旋转 90度
+        // 缩放 后面的三个参数分别代表 x, y, z 轴的缩放比例
+        trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 1.0f));
+        // 旋转 后面的三个参数分别代表 x, y, z 轴对应前面的度数，旋转的百分比
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        // 位移 后面的三个参数分别代表 x, y, z 轴相对于顶点坐标进行的位移值
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+        
+        unsigned int tramsFormLoc = glGetUniformLocation(ourShader.ID, "transform");
+        glUniformMatrix4fv(tramsFormLoc, 1, GL_FALSE, glm::value_ptr(trans));
+        
         glBindVertexArray(VAO);
         // 6 是顶点数量
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
