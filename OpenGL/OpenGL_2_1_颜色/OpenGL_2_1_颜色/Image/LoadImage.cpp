8f2045117068671156814bce38266e1920028157
Dzy <dzy8023@163.com>
2019-09-19 09:52:57 +0800

    2_1_颜色 搭了个之后用的框架

---
+++ OpenGL/OpenGL_2_1_颜色/OpenGL_2_1_颜色/Image/LoadImage.cpp
@@ -0,0 +1,10 @@
+//
+//  LoadImage.cpp
+//  OpenGL_4_纹理
+//
+//  Created by edz on 2019/9/9.
+//  Copyright © 2019 灰s. All rights reserved.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
